3b248c01e483e19046339d2a99d2705acbd63364
flora <flora5.bai@gmail.com>
2016-10-12 13:37:12 +0800

    response html with var

--- blog/app1/views.py
+++ blog/app1/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+#return pure string
+def index_string(request):
+    return HttpResponse("Hello, this is index...")
+
+#return html
+def index(request):
+    data = "Hello, i'm come from mysql"
+    return render(request,'index.html',{'data': data})
